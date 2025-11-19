db6d3ae953be9185d5450393ddf6e768d9695a1a
nuricanacar <nuri.can60@outlook.com>
2025-11-19 13:47:15 +0300

    Offer search updated

--- src/app/components/offer-search/offer-search.ts
+++ src/app/components/offer-search/offer-search.ts
@@ -43,8 +43,9 @@ export class OfferSearch implements OnInit {
   selectedBillingAccountId = this.customerStateService.selectedBillingAccountId;
   currentCampaignId: number | undefined;
 
-  isSearchDisabled = computed(() => {
-    return false;
+isSearchDisabled = computed(() => {
+    const val = this.searchForm?.value; 
+    return true;
   });
 
   ngOnInit(): void {
@@ -58,9 +59,135 @@ export class OfferSearch implements OnInit {
       campaignName: [''], // Bu "Prod Offer Name" olarak kullanılıyor,
     });
 
+
+    this.listenToDropdownChanges();
+
     this.loadCatalogs();
     this.loadCampaigns();
-    this.listenToDropdownChanges();
+    
+    this.setupCatalogListeners();
+    this.setupCampaignListeners();
+  }
+
+  // --- CATALOG LISTENERLARI ---
+  setupCatalogListeners() {
+    const catSelect = this.searchForm.get('catalogSelection');
+    const catIdInput = this.searchForm.get('catalogOfferId');
+    const catNameInput = this.searchForm.get('catalogOfferName');
+
+    // 1. Catalog Seçimi Değişince
+    catSelect?.valueChanges.subscribe(val => {
+      if (val) {
+        // Seçim yapıldıysa inputları aç (eğer diğeri dolu değilse)
+        if (!catNameInput?.value) catIdInput?.enable({ emitEvent: false });
+        if (!catIdInput?.value) catNameInput?.enable({ emitEvent: false });
+        
+        // Otomatik listeleme (İsteğe bağlı, önceki koddan)
+        this.searchCatalogOffers(Number(val));
+      } else {
+        // Seçim kalkarsa hepsini kapat ve temizle
+        catIdInput?.disable({ emitEvent: false });
+        catNameInput?.disable({ emitEvent: false });
+        catIdInput?.setValue('', { emitEvent: false });
+        catNameInput?.setValue('', { emitEvent: false });
+        this.searchResults.set([]);
+      }
+    });
+
+    // 2. ID Input Değişince (Name'i Kilitle)
+    catIdInput?.valueChanges.subscribe(val => {
+      if (val && val.toString().trim() !== '') {
+        catNameInput?.disable({ emitEvent: false });
+      } else {
+        // Eğer katalog seçiliyse tekrar aç
+        if (catSelect?.value) catNameInput?.enable({ emitEvent: false });
+      }
+    });
+
+    // 3. Name Input Değişince (ID'yi Kilitle)
+    catNameInput?.valueChanges.subscribe(val => {
+      if (val && val.trim() !== '') {
+        catIdInput?.disable({ emitEvent: false });
+      } else {
+        // Eğer katalog seçiliyse tekrar aç
+        if (catSelect?.value) catIdInput?.enable({ emitEvent: false });
+      }
+    });
+  }
+
+  // --- CAMPAIGN LISTENERLARI ---
+  setupCampaignListeners() {
+    const campSelect = this.searchForm.get('campaignSelection');
+    const campManualId = this.searchForm.get('campaignId');
+    const campOfferId = this.searchForm.get('campaignOfferId');
+    const campOfferName = this.searchForm.get('campaignName');
+
+    // Yardımcı: Kampanya seçili mi? (Dropdown veya Manuel ID)
+    const isCampaignSelected = () => !!campSelect?.value || !!campManualId?.value;
+
+    // Dropdown veya Manuel ID değişince inputları yönet
+    const handleCampaignSelectionChange = () => {
+      const hasSelection = isCampaignSelected();
+      if (hasSelection) {
+        // Kampanya belliyse inputları aç (kilitli değilse)
+        if (!campOfferName?.value) campOfferId?.enable({ emitEvent: false });
+        if (!campOfferId?.value) campOfferName?.enable({ emitEvent: false });
+
+        // Otomatik listeleme
+        let cId = campSelect?.value ? Number(campSelect?.value) : (campManualId?.value ? Number(campManualId.value) : undefined);
+        if (cId) {
+            this.currentCampaignId = cId;
+            this.searchCampaignOffers(cId);
+        }
+      } else {
+        // Seçim yoksa kapat
+        campOfferId?.disable({ emitEvent: false });
+        campOfferName?.disable({ emitEvent: false });
+        campOfferId?.setValue('', { emitEvent: false });
+        campOfferName?.setValue('', { emitEvent: false });
+        this.searchResults.set([]);
+      }
+    };
+
+    campSelect?.valueChanges.subscribe(handleCampaignSelectionChange);
+    campManualId?.valueChanges.subscribe(handleCampaignSelectionChange);
+
+    // ID Input Değişince (Name'i Kilitle)
+    campOfferId?.valueChanges.subscribe(val => {
+      if (val && val.toString().trim() !== '') {
+        campOfferName?.disable({ emitEvent: false });
+      } else {
+        if (isCampaignSelected()) campOfferName?.enable({ emitEvent: false });
+      }
+    });
+
+    // Name Input Değişince (ID'yi Kilitle)
+    campOfferName?.valueChanges.subscribe(val => {
+      if (val && val.trim() !== '') {
+        campOfferId?.disable({ emitEvent: false });
+      } else {
+        if (isCampaignSelected()) campOfferId?.enable({ emitEvent: false });
+      }
+    });
+  }
+
+  // HTML Tarafında [disabled] için kullanılacak metot
+  checkSearchDisabled(): boolean {
+    const val = this.searchForm.getRawValue(); // Disabled alanları da okumak için getRawValue
+
+    if (this.activeTab() === 'catalog') {
+      const hasCatalog = !!val.catalogSelection;
+      const hasId = !!val.catalogOfferId;
+      const hasName = !!val.catalogOfferName;
+      // Catalog seçili olmalı VE (ID dolu VEYA Name dolu olmalı)
+      return !(hasCatalog && (hasId || hasName));
+    } else {
+      const hasCampaign = !!val.campaignSelection || !!val.campaignId;
+      const hasOfferId = !!val.campaignOfferId;
+      const hasOfferName = !!val.campaignName;
+      // Kampanya seçili olmalı VE (ID dolu VEYA Name dolu olmalı)
+      return !(hasCampaign && (hasOfferId || hasOfferName));
+    }
   }
 
   loadCatalogs(): void {
@@ -188,7 +315,14 @@ export class OfferSearch implements OnInit {
   setTab(tab: 'catalog' | 'campaign') {
     this.activeTab.set(tab);
     this.searchResults.set([]);
-    this.searchForm.get('catalogSelection')?.setValue('');
-    this.searchForm.get('campaignSelection')?.setValue('');
-    }
+    
+    // Tab değişince form resetleme ve disable duruma getirme
+    this.searchForm.reset();
+    
+    // Reset sonrası tekrar disable et (çünkü reset enable yapabilir)
+    this.searchForm.get('catalogOfferId')?.disable();
+    this.searchForm.get('catalogOfferName')?.disable();
+    this.searchForm.get('campaignOfferId')?.disable();
+    this.searchForm.get('campaignName')?.disable();
+  }
 }
